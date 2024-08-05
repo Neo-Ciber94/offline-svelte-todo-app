66a93c8595de0e89bf8cfccd12f2a787f019e51d
Freddy L <neo-ciber_94@hotmail.com>
2024-08-05 19:27:23 -0400

    Add error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="w-full h-screen flex flex-row items-center justify-center">
+	<h1 class="text-red-500 font-bold text-xl">
+		<span>{$page.status}</span>
+		<span class="opacity-70">|</span>
+		<span>{$page.error?.message ?? 'Something went wrong'}</span>
+	</h1>
+</div>
