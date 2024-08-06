9d69d53dd13b2b795b82b6842c47d5c02d45890f
Freddy L <neo-ciber_94@hotmail.com>
2024-08-05 22:15:21 -0400

    Add service worker caching

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full h-screen flex flex-row justify-center items-center">
+<div class="w-full h-screen flex flex-col justify-center items-center">
 	<a href="/todos">Go to Todos</a>
 	<a href="/login">Go to Login</a>
 </div>
