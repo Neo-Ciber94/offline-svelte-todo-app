04cc10a7c137168136d3536a518f544b5a93bbc9
Freddy L <neo-ciber_94@hotmail.com>
2024-08-06 19:54:08 -0400

    Create local db only on browser

---
+++ app/src/lib/dal/local-db.ts
@@ -0,0 +1,35 @@
+import { browser } from '$app/environment';
+import { pendingTodoSchema, todoSchema, userSchema } from '$lib/data';
+import { BrowserDatabase } from 'browser-idb';
+
+function createBrowserDb() {
+	function create() {
+		return new BrowserDatabase({
+			version: 1,
+			definition: {
+				todos: {
+					keys: ['id'],
+					schema: todoSchema
+				},
+				users: {
+					keys: [],
+					schema: userSchema
+				},
+				pendingTodos: {
+					keys: ['id'],
+					schema: pendingTodoSchema
+				}
+			}
+		});
+	}
+
+	type LocalDb = ReturnType<typeof create>;
+
+	if (!browser) {
+		return {} as LocalDb;
+	}
+
+	return create();
+}
+
+export const db = createBrowserDb();
