b15fc2866296d28b076ebf8b51545f2da4327708
Freddy L <neo-ciber_94@hotmail.com>
2024-08-10 11:48:12 -0400

    minor changes

--- app/src/lib/common/constants.ts
+++ app/src/lib/common/constants.ts
@@ -1,2 +1,2 @@
 export const COOKIE_AUTH_TOKEN = 'auth-token';
-export const PUBLIC_ROUTES = ['/login', '/register', '/offline'] as const;
+export const PUBLIC_ROUTES = ['/', '/login', '/register', '/offline'] as const;
