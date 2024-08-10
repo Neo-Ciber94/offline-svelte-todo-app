87dc0559d7db6b9648232252c558c823ad0b2570
Freddy L <neo-ciber_94@hotmail.com>
2024-08-10 14:54:36 -0400

    Update home page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -10,19 +10,35 @@
 		queryKey: queryKeys.users.me(),
 		queryFn: () => userService.getCurrentUser()
 	});
+
+	let visible = $state(false);
+
+	$effect(() => {
+		visible = true;
+	});
 </script>
 
 <div class="w-full h-[calc(100vh-var(--header-height))] flex flex-col justify-center items-center">
 	<div class="max-w-3xl px-4 md:px-6 text-center">
-		<h1 class="text-xl sm:text-3xl md:text-6xl font-bold mb-4">
+		<h1
+			data-visible={visible}
+			class={`text-xl sm:text-3xl md:text-6xl font-bold mb-4 
+					-translate-y-10 opacity-0 data-[visible=true]:opacity-100 data-[visible=true]:translate-y-0 transition duration-1000 delay-0`}
+		>
 			Never Lose Your Tasks - Our Offline-First Todo App Has Got You Covered
 		</h1>
-		<p class="text-base md:text-lg mb-8">
+		<p
+			data-visible={visible}
+			class="text-base md:text-lg mb-8 -translate-x-10 data-[visible=true]:translate-x-0 opacity-0 data-[visible=true]:opacity-100 transition duration-1000 delay-200"
+		>
 			Access your tasks anytime, anywhere, even without an internet connection.
 		</p>
 		<a
-			class="inline-flex items-center justify-center h-12 px-8 min-w-44 rounded-md text-white font-medium bg-green-500 hover:bg-green-600 text-lg transition-colors focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2"
+			data-visible={visible}
 			href={$userQuery.data ? '/todos' : '/login'}
+			class={`inline-flex items-center justify-center h-12 px-8 min-w-44 rounded-md text-white font-medium bg-green-500 hover:bg-green-600 text-lg 
+					opacity-0 data-[visible=true]:opacity-100 transition duration-1000 delay-200 data-[visible=true]:translate-y-0 translate-y-5 animate-grow
+					focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2`}
 		>
 			{#if $userQuery.isLoading}
 				<Loading class="size-5" />
@@ -34,3 +50,19 @@
 		</a>
 	</div>
 </div>
+
+<style>
+	@keyframes growAnimation {
+		0%,
+		100% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.05);
+		}
+	}
+
+	.animate-grow:hover {
+		animation: growAnimation 1s infinite;
+	}
+</style>
