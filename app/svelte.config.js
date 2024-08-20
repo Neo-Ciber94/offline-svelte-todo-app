d5cc59927108d35fe7178d9e196bf2d70e852536
Freddy L <neo-ciber_94@hotmail.com>
2024-08-19 20:57:58 -0400

    Prevent relative paths for csr

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -11,6 +11,9 @@ const config = {
 		serviceWorker: {
 			register: false
 		},
+		paths: {
+			relative: false
+		},
 
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
